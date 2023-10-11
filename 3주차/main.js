fc14a955edf7e5a44e29e4b27edc598081cc4ea3
Oseojin <77391201+Oseojin@users.noreply.github.com>
2023-10-11 16:42:17 +0900

    3Week Time 6

---
+++ 3주차/main.js
@@ -0,0 +1,14 @@
+let radius = 5;
+let area = 0.0;
+const pi = 3.14;
+
+{
+    let radius = 1;
+    let area = 0.0;
+    
+    area = radius * radius * pi;
+
+    console.log(area);
+}
+
+console.log(area);
